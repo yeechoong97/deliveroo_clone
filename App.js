51b79c89902d114cb59f0edb73e1b1a8694c64e6
Yee Choong <yeechoong97@gmail.com>
2022-07-10 11:06:13 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+
+  return (
+    <View>
+      <Text className="text-red-500">800</Text>
+    </View>
+  );
+};
+
+
+
+export default App;
