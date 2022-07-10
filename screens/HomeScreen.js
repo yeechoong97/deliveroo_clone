64bbeabcab6ea24204172a5034c8a0b16ead3281
Yee Choong <yeechoong97@gmail.com>
2022-07-10 12:00:53 +0800

    Added Search Component Part

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from "@react-navigation/native"
 import { ChevronDownIcon, UserIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
@@ -27,6 +27,16 @@ const HomeScreen = () => {
                 </View>
                 <UserIcon size={35} color="#00CCBB" />
             </View>
+
+            {/* Search */}
+            <View className="flex-row items-center space-x-2 pb-2 px-4">
+                <View className="flex-row space-x-2 bg-gray-200 flex-1 items-center px-4">
+                    <SearchIcon size={20} color="gray" />
+                    <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+                </View>
+                <AdjustmentsIcon size={24} color="#00CCBB" />
+            </View>
+
         </SafeAreaView>
     )
 }
