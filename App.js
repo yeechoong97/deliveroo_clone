6b34b2e80011694766c1ada3d16bed5ae82e9587
Yee Choong <yeechoong97@gmail.com>
2022-07-10 11:26:10 +0800

    Setup the react navigation

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import HomeScreen from './screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 
   return (
-    <View>
-      <Text className="text-red-500">800</Text>
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 };
 
