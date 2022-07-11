af24c31f9758d4d79e66419ce7c392ee748d2ee1
Yee Choong <yeechoong97@gmail.com>
2022-07-11 12:11:43 +0800

    Implemented Sanity Backend

---
+++ sanity.js
@@ -0,0 +1,15 @@
+import { SanityClient } from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url";
+
+const client = SanityClient({
+    projectId: "dobjz2yr",
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2021-10-21",
+})
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+export default client;
