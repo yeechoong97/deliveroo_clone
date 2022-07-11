a68c0e0cf6feb7523f4aec7a5a1af448af2c19b6
Yee Choong <yeechoong97@gmail.com>
2022-07-11 17:46:53 +0800

    Implemented Redux

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import store from './redux/store';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 
@@ -12,12 +14,14 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <ReduxProvider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </ReduxProvider>
     </NavigationContainer>
   );
 };
